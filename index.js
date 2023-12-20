b240782a90e634f9b5d7dc0726d02f0a7db2813b
Saad <educationalmindz@gmail.com>
2023-12-19 20:45:34 -0500

    added chessboard background color black

--- index.js
+++ index.js
@@ -3,8 +3,22 @@ const playerDisplay = document.querySelector("#player")
 const width = 8
 
 const startPieces = [
-
-
-
-    
+    rook, knight, bishop, queen, king, bishop, knight, rook,
+    pawn, pawn, pawn, pawn, pawn, pawn, pawn, pawn,
+    '', '', '', '', '', '', '', '',
+    '', '', '', '', '', '', '', '',
+    '', '', '', '', '', '', '', '',
+    '', '', '', '', '', '', '', '',
+    pawn, pawn, pawn, pawn, pawn, pawn, pawn, pawn,
+    rook, knight, bishop, queen, king, bishop, knight, rook
 ]
+
+function createBoard(){ 
+    startPieces.forEach((startPiece) => {
+       const square = document.createElement('div')
+       square.classList.add('square')
+    })
+
+}
+
+

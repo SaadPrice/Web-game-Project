305e430fcfb5b90bf296eb41d5ea8f2861729fc1
Saad <educationalmindz@gmail.com>
2023-12-21 21:52:24 -0500

    added my pieces and the square colors to my chessboard.

--- index.js
+++ index.js
@@ -18,9 +18,15 @@ function createBoard() {
        const square = document.createElement('div')
        square.classList.add('square')
        square.setAttribute('square-id', i)
-       square.classList.add('red')
+      // square.classList.add('red')
        square.innerHTML = startPiece
-       chessBoard.append(square)
+       const row = Math.floor ( (63 - i) / 8) + 1
+       if  ( row % 2 === 0) {
+            square.classList.add(i % 2 === 0 ? "blue" : "red")
+       } else{
+        square.classList.add(i % 2 === 0 ? "red" : "blue")
+       }
+       chessBoard.append(square) 
     
     })
 
